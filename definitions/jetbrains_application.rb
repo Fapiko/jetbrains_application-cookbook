3841477471ab753bcab97b1f3d358467836d290b
Lucas Jandrew <lucas@jandrew.com>
2013-01-24 00:23:02 -0800

    Fixing a typo

--- definitions/jetbrains_application.rb
+++ definitions/jetbrains_application.rb
@@ -7,7 +7,7 @@
 # All rights reserved - Do Not Redistribute
 #
 
-include_reciipe 'java'
+include_recipe 'java'
 require 'base64'
 
 define :jetbrains_application, :internal_name => 'WebIde', :major_version => 5 do
