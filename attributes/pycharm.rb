2c7ea4a4db09094e951ef5d95aab41ab89df9670
Lucas Jandrew <lucas@jandrew.com>
2013-02-22 23:19:43 -0800

    Updating default pycharm version

--- attributes/pycharm.rb
+++ attributes/pycharm.rb
@@ -7,10 +7,10 @@
 # All rights reserved - Do Not Redistribute
 #
 
-default[:applications][:jetbrains][:pycharm][:version] = '2.6.2'
+default[:applications][:jetbrains][:pycharm][:version] = '2.7'
 
 default[:applications][:jetbrains][:pycharm][:url] = "http://download.jetbrains.com/python/pycharm-#{node[:applications][:jetbrains][:pycharm][:version]}.tar.gz"
-default[:applications][:jetbrains][:pycharm][:checksum] = 'e0b334589ef06969fb0a2c59a1a78a6ca55805a5a1faf675d9748931fb3d71e0'
+default[:applications][:jetbrains][:pycharm][:checksum] = '4161e1488cc14429695274de8b6594f7fca4c8a88714e3ddb795fee95afd4f30'
 
 default[:applications][:jetbrains][:pycharm][:dir] = '/opt/pycharm'
 
